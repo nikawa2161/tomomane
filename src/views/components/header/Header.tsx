472cfaa550cccfb3d2bd6596ffff0f5a555c8cff
nikawadori <nikawa.2161.t@gmail.com>
2022-10-18 23:47:55 +0900

    ハンバーガーメニュー位置の変更

--- src/views/components/header/Header.tsx
+++ src/views/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import './sidebar.css'
+import './hamburger.css'
 
 import Sidebar from "./Sidebar"
 
@@ -8,7 +8,7 @@ function Header() {
       <Sidebar />
       <header className="">
         <div className="py-4 px-4 bg-blue-primary regal-blue">
-          <div className="flex justify-end">
+          <div className="flex justify-start">
             <img
               className="h-5"
               src={`${process.env.PUBLIC_URL}/images/header/logo1.png`}
