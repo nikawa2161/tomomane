0ac48460503e14bc837fef9e17b206a592fbf707
nikawadori <nikawa.2161.t@gmail.com>
2022-10-23 16:14:03 +0900

    アイコンメニュー、map化

--- src/views/pages/common/header/IconMenuList.tsx
+++ src/views/pages/common/header/IconMenuList.tsx
@@ -1,64 +1,35 @@
-import { FC } from "react"
-import { Link } from "react-router-dom"
+import { FC } from 'react'
+import { IconRoutes } from './IconRoutes'
 
-export const IconMenuList:FC = () => {
+import { Link } from 'react-router-dom'
+
+export const IconMenuList: FC = () => {
+  type RoutesItem = {
+    path: string
+    imageUrl: string
+    title: string
+  }
+
   return (
     <>
       <div className="px-4 pb-4 pt-2 bg-blue-primary regal-blue md:hidden">
         <ul className="flex justify-between">
-          <li className="flex items-center flex-col">
-            <Link to={`/`}>
-              <img
-                className="h-5 mx-auto my-0"
-                src={`${process.env.PUBLIC_URL}/images/icons/book1.png`}
-                alt=""
-              />
-              <p className="mt-1 text-gray-400 text-sm">トモダチ</p>
-            </Link>
-          </li>
-          <li className="flex items-center flex-col">
-            <Link to={`/post/`}>
-              <img
-                className="h-5 mx-auto my-0"
-                src={`${process.env.PUBLIC_URL}/images/icons/men1.png`}
-                alt=""
-              />
-              <p className="mt-1 text-gray-400 text-sm">友達追加</p>
-            </Link>
-          </li>
-          <li className="flex items-center flex-col">
-            <Link to={`/like/`}>
-              <img
-                className="h-5 mx-auto my-0"
-                src={`${process.env.PUBLIC_URL}/images/icons/star1.png`}
-                alt=""
-              />
-              <p className="mt-1 text-gray-400 text-sm">お気に入り</p>
-            </Link>
-          </li>
-          <li className="flex items-center flex-col">
-            <Link to={`/calendar/`}>
-              <img
-                className="h-5 mx-auto my-0"
-                src={`${process.env.PUBLIC_URL}/images/icons/calendar1.png`}
-                alt=""
-              />
-              <p className="mt-1 text-gray-400 text-sm">カレンダー</p>
-            </Link>
-          </li>
-          <li className="flex items-center flex-col">
-            <Link to={`/account/`}>
-              <img
-                className="h-5 mx-auto my-0"
-                src={`${process.env.PUBLIC_URL}/images/icons/human2.png`}
-                alt=""
-              />
-              <p className="mt-1 text-gray-400 text-sm">アカウント</p>
-            </Link>
-          </li>
+          {IconRoutes.map((route: RoutesItem) => {
+            return (
+              <li key={route.path} className="flex items-center flex-col">
+                <Link to={route.path}>
+                  <img
+                    className="h-5 mx-auto my-0"
+                    src={`${process.env.PUBLIC_URL}${route.imageUrl}`}
+                    alt=""
+                  />
+                  <p className="mt-1 text-gray-400 text-sm">{route.title}</p>
+                </Link>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </>
   )
 }
-
