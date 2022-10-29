079df3171470779db8ead7110d27bfc50cf8edbd
nikawadori <nikawa.2161.t@gmail.com>
2022-10-29 14:46:07 +0900

    TopUIリファクタリング

--- src/views/pages/home/Home.tsx
+++ src/views/pages/home/Home.tsx
@@ -1,18 +1,26 @@
-import { FC } from "react"
+import { FC } from 'react'
+import { friendData } from 'testData'
 
-export const Home:FC = () => {
+export const Home: FC = () => {
+  type FriendData = {
+    id: number
+    name: string
+    image: string
+    category: string
+  }
   return (
     <>
-      <div className="flex justify-between py-5 px-5 items-center border-solid border-b border-current">
+      <div className="flex justify-between py-5 px-5 items-center border-solid border-b border-current text-blue-primary">
         <p className="text-2xl">山田太郎</p>
         <img
+        
           className="h-12"
           src={`${process.env.PUBLIC_URL}/images/sample/1.png`}
           alt=""
         />
       </div>
       <div className="my-2 mx-2 py-1 px-1 rounded-2xl bg-slate-100">
-        <div className="px-4 flex items-center">
+        <div className="px-4 flex items-center text-blue-primary">
           <img
             className="h-3"
             src={`${process.env.PUBLIC_URL}/images/icons/search1.png`}
@@ -22,24 +30,31 @@ export const Home:FC = () => {
           <input className="flex-1 ml-3 bg-transparent" type="text" />
         </div>
       </div>
-      <div className="py-2 px-5 flex justify-between">
+      <div className="py-2 px-5 flex justify-between text-blue-primary">
         <p className="py-1 border-solid border-b border-current">友達一覧</p>
         <p className="py-1">カテゴリー</p>
         <p className="py-1">お気に入り</p>
       </div>
       <div className="">
         <ul className="px-5">
-          <li className="flex items-center mt-6">
-            <img
-              className="rounded-xl h-16"
-              src={`${process.env.PUBLIC_URL}/images/sample/1.png`}
-              alt=""
-            />
-            <div className="ml-4">
-              <p className="text-2xl">鈴木花子</p>
-              <p className="text-xl">カテゴリー:仕事</p>
-            </div>
-          </li>
+          {friendData.map((friend: FriendData) => {
+            return (
+              <li
+                key={friend.id}
+                className="flex items-center mt-6 text-blue-primary"
+              >
+                <img
+                  className="rounded-xl h-16"
+                  src={`${process.env.PUBLIC_URL}${friend.image}`}
+                  alt=""
+                />
+                <div className="ml-4">
+                  <p className="text-2xl">{friend.name}</p>
+                  <p className="text-xl">カテゴリー:{friend.category}</p>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </>
