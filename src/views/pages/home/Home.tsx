9185edb362abc087f14a2a705629fd4119c3a026
nikawadori <nikawa.2161.t@gmail.com>
2022-10-31 22:40:08 +0900

    メニューボタンの切り替え実装

--- src/views/pages/home/Home.tsx
+++ src/views/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { friendData } from 'testData'
 
 export const Home: FC = () => {
@@ -8,12 +8,24 @@ export const Home: FC = () => {
     image: string
     category: string
   }
+
+  const [choice, setChoice] = useState("friend");
+
+  const friendHandler = () => {
+    setChoice('friend')
+  }
+  const categoryHandler = () => {
+    setChoice('category')
+  }
+  const likeHandler = () => {
+    setChoice('like')
+  }
+
   return (
     <>
       <div className="flex justify-between py-5 px-5 items-center border-solid border-b border-current text-blue-primary">
         <p className="text-2xl">山田太郎</p>
         <img
-        
           className="h-12"
           src={`${process.env.PUBLIC_URL}/images/sample/1.png`}
           alt=""
@@ -31,17 +43,39 @@ export const Home: FC = () => {
         </div>
       </div>
       <div className="py-2 px-5 flex justify-between text-blue-primary">
-        <p className="py-1 border-solid border-b border-current">友達一覧</p>
-        <p className="py-1">カテゴリー</p>
-        <p className="py-1">お気に入り</p>
+        <button
+          onClick={friendHandler}
+          className={`py-1 border-solid  border-current ${
+            choice === 'friend' ? 'border-b' : ''
+          }`}
+        >
+          友達一覧
+        </button>
+        <button
+          onClick={categoryHandler}
+          className={`py-1 border-solid  border-current ${
+            choice === 'category' ? 'border-b' : ''
+          }`}
+        >
+          カテゴリー
+        </button>
+        <button
+          onClick={likeHandler}
+          className={`py-1 border-solid  border-current ${
+            choice === 'like' ? 'border-b' : ''
+          }`}
+        >
+          お気に入り
+        </button>
       </div>
-      <div className="">
+
+      <div className="mt-5">
         <ul className="px-5">
           {friendData.map((friend: FriendData) => {
             return (
               <li
                 key={friend.id}
-                className="flex items-center mt-6 text-blue-primary"
+                className="flex items-center mt-6 text-blue-primary first:mt-0"
               >
                 <img
                   className="rounded-xl h-16"
