5deadc16d3085ef7d56ef44b70903208e6071466
nikawadori <nikawa.2161.t@gmail.com>
2022-11-09 22:27:59 +0900

    お気に入りユーザーの表示

--- src/views/pages/home/LikeHome.tsx
+++ src/views/pages/home/LikeHome.tsx
@@ -1,5 +1,42 @@
 import { FC } from 'react'
+import { friendData } from 'testData'
 
 export const LikeHome: FC = () => {
-  return <p>お気に入り</p>
+  type FriendData = {
+    id: number
+    name: string
+    image: string
+    category: string
+    favorite: boolean
+  }
+  return (
+    <>
+      <ul>
+        {friendData.map(
+          (friend: FriendData) =>
+            friend.favorite === true && (
+              <li
+                key={friend.id}
+                className="relative flex items-center mt-6 text-blue-primary first:mt-0"
+              >
+                <img
+                  className="rounded-xl h-16"
+                  src={`${process.env.PUBLIC_URL}${friend.image}`}
+                  alt=""
+                />
+                <div className="ml-4">
+                  <p className="text-2xl">{friend.name}</p>
+                  <p className="text-xl">カテゴリー:{friend.category}</p>
+                </div>
+                <img
+                  className="h-10 absolute right-0"
+                  src={`${process.env.PUBLIC_URL}/images/icons/star2.png`}
+                  alt=""
+                />
+              </li>
+            ),
+        )}
+      </ul>
+    </>
+  )
 }
