f2a4d246e63db6426d999afbee92055a43e067e0
nikawadori <nikawa.2161.t@gmail.com>
2022-11-11 21:15:37 +0900

    テストデザイン作成

--- src/views/pages/home/CategoryHome.tsx
+++ src/views/pages/home/CategoryHome.tsx
@@ -1,5 +1,21 @@
 import { FC } from 'react'
 
 export const CategoryHome: FC = () => {
-  return <p>カテゴリー</p>
+  return (
+    <>
+      <div className="flex flex-wrap justify-between ">
+        {/* カード */}
+        <div className="w-5/12 rounded-xl truncate shadow-md border border-solid border-blue-900  ">
+          <a href="/">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/sample/category1.jpg`}
+              alt="カテゴリー画像"
+              className="h-36 object-cover w-full"
+            />
+            <p className="py-2 text-center text-sm">仕事の同僚</p>
+          </a>
+        </div>
+      </div>
+    </>
+  )
 }
