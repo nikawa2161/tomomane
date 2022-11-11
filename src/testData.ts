f52ead3a2b57af6b16b620d39d3af8d45e22e244
nikawadori <nikawa.2161.t@gmail.com>
2022-11-11 21:26:29 +0900

    画像データ追加、テストデータの表示

--- src/testData.ts
+++ src/testData.ts
@@ -21,6 +21,7 @@ type FriendData = {
 type CategoryData = {
   id: number
   name: string
+  image: string
 }
 
 export const friendData: FriendData[] = [
@@ -87,17 +88,21 @@ export const categoryData: CategoryData[] = [
   {
     id: 1,
     name: '無所属',
+    image: '/images/sample/category1.jpg'
   },
   {
     id: 2,
     name: '友人',
+    image: '/images/sample/category2.jpg'
   },
   {
     id: 3,
     name: 'エンジニア',
+    image: '/images/sample/category3.jpg'
   },
   {
     id: 4,
     name: '仕事の同僚',
+    image: '/images/sample/category4.jpg'
   },
 ]
--- src/views/pages/home/CategoryHome.tsx
+++ src/views/pages/home/CategoryHome.tsx
@@ -1,20 +1,33 @@
 import { FC } from 'react'
+import { categoryData } from 'testData'
 
 export const CategoryHome: FC = () => {
+  type CategoryData = {
+    id: number
+    name: string
+    image: string
+  }
   return (
     <>
       <div className="flex flex-wrap justify-between ">
         {/* カード */}
-        <div className="w-5/12 rounded-xl truncate shadow-md border border-solid border-blue-900  ">
-          <a href="/">
-            <img
-              src={`${process.env.PUBLIC_URL}/images/sample/category1.jpg`}
-              alt="カテゴリー画像"
-              className="h-36 object-cover w-full"
-            />
-            <p className="py-2 text-center text-sm">仕事の同僚</p>
-          </a>
-        </div>
+        {categoryData.map((category: CategoryData) => {
+          return (
+            <div
+              className="w-40 mb-5 rounded-xl truncate shadow-md border border-solid border-blue-900"
+              key={category.id}
+            >
+              <a href="/">
+                <img
+                  src={`${process.env.PUBLIC_URL}${category.image}`}
+                  alt="カテゴリー画像"
+                  className="h-40 object-cover w-full"
+                />
+                <p className="py-2 text-center text-sm">{category.name}</p>
+              </a>
+            </div>
+          )
+        })}
       </div>
     </>
   )
