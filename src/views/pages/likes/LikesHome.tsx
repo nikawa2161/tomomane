4a405e0012ff0ba994e1a896cc8478b6d1c2e244
nikawadori <nikawa.2161.t@gmail.com>
2022-11-12 18:28:34 +0900

    お気に入りUIの作成

--- src/views/pages/likes/LikesHome.tsx
+++ src/views/pages/likes/LikesHome.tsx
@@ -1,6 +1,33 @@
 import { FC } from 'react'
+import { LikeHome } from '../home/LikeHome'
 
 export const LikesHome: FC = () => {
-  return <div>LikeHome</div>
+  return (
+    <>
+      <div className="p-5">
+        <h2 className="text-2xl">お気に入り</h2>
+        <div className="mt-5 pb-1 flex justify-between border-b solid border-current text-sm">
+          <p>○件</p>
+          <div className="relative">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/icons/filter1.png`}
+              alt="ソート"
+              className="h-4 absolute  -translate-y-2/4 top-1/2"
+            />
+            <button id="" className="w-auto pl-5 text-right">
+              並び替え
+            </button>
+            <ul className="hidden">
+              <li>新しいお気に入り順</li>
+              <li>古いお気に入り順</li>
+              <li>名前順</li>
+            </ul>
+          </div>
+        </div>
+        <div className='mt-5'>
+          <LikeHome />
+        </div>
+      </div>
+    </>
+  )
 }
-
