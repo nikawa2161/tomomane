25cf0f096d8f71f264070f186a55f3554371171c
nikawadori <nikawa.2161.t@gmail.com>
2022-11-20 00:10:13 +0900

    プロフィールフォームの作成

--- src/views/pages/account/AccountHome.tsx
+++ src/views/pages/account/AccountHome.tsx
@@ -1,5 +1,68 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { inputProfile } from './inputFrom'
 
 export const AccountHome: FC = () => {
-  return <div>AccountHome</div>
+  const [isProfile, setIsProfile] = useState(true)
+
+  const profileHandler = () => {
+    setIsProfile(true)
+  }
+  const memoHandler = () => {
+    setIsProfile(false)
+  }
+
+  type inputType = {
+    id: number
+    label: string
+    placeholder: string
+  }
+
+  return (
+    <>
+      <div className="p-5 text-blue-primary">
+        <h2 className="text-2xl">プロフィール</h2>
+        <div className="relative mt-10 pb-1 text-sm ">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/sample/1.png`}
+            alt="プロフィール画像"
+            className="h-28 mx-auto rounded-xl"
+          />
+          <p className="mt-4 text-center text-2xl">山田太郎</p>
+        </div>
+        {/* 以下、共通化対象 */}
+        <div className="mt-5 p-2 flex justify-between bg-slate-400 w-9/12 rounded-xl mx-auto text-blue-primary font-bold">
+          <button
+            onClick={profileHandler}
+            className={`w-6/12  text-center rounded-xl ${
+              isProfile ? 'bg-gray-50' : ''
+            }`}
+          >
+            プロフィール
+          </button>
+          <button
+            onClick={memoHandler}
+            className={`w-6/12  text-center rounded-xl ${
+              !isProfile ? 'bg-gray-50' : ''
+            }`}
+          >
+            ログイン情報
+          </button>
+        </div>
+
+        {/* プロフィールフォーム */}
+        <div className="mt-5">
+          {inputProfile.map((input: inputType) => (
+            <div className="mt-5 flex justify-between" key={input.id}>
+              <p className="font-bold w-28 text-blue-primary">{input.label}</p>
+              <input
+                className="flex-1"
+                type="text"
+                placeholder={input.placeholder}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
---
+++ src/views/pages/account/inputFrom.ts
@@ -0,0 +1,53 @@
+type InputType = {
+  id: number
+  label: string
+  placeholder: string
+}
+
+export const inputProfile: InputType[] = [
+  {
+    id: 1,
+    label: '性別',
+    placeholder: '男性',
+  },
+  {
+    id: 2,
+    label: '誕生日',
+    placeholder: '2000年1月1日',
+  },
+  {
+    id: 3,
+    label: '住所',
+    placeholder: '東京',
+  },
+  {
+    id: 4,
+    label: '職業',
+    placeholder: 'エンジニア',
+  },
+  {
+    id: 5,
+    label: '電話番号',
+    placeholder: '090-1234-5678',
+  },
+  {
+    id: 6,
+    label: 'Twitter',
+    placeholder: '@tomomane',
+  },
+  {
+    id: 7,
+    label: 'instagram',
+    placeholder: '@tomomane',
+  },
+  {
+    id: 8,
+    label: 'TikTok',
+    placeholder: '@tomomane',
+  },
+  {
+    id: 9,
+    label: 'YouTube',
+    placeholder: 'トモマネチャンネル',
+  },
+]
