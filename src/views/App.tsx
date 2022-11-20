3cd5f9a639397fd88cb8a816c44393ef64244935
nikawadori <nikawa.2161.t@gmail.com>
2022-11-20 17:16:04 +0900

    ログインUIの作成

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react'
+import { AuthHome } from './pages/authentication/AuthHome'
 import { Header } from './pages/common/header/Header'
-import { Router } from '../router/Router'
+// import { Router } from '../router/Router'
 
 export const App: FC = () => {
   return (
     <>
-      <div className="App">
+      <div className="App h-screen">
         <Header />
-        <Router />
+        {/* <Router /> */}
+        <AuthHome />
       </div>
     </>
   )
---
+++ src/views/pages/authentication/AuthHome.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+
+export const AuthHome: FC = () => {
+  return (
+    <>
+      <div className="p-5 h-3/4">
+        <div className="h-full flex items-center flex-col justify-center pb-1 text-sm">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/icons/book1.png`}
+            alt="ソート"
+            className="h-28"
+          />
+          <p className="mt-5 text-center">ログインが必要です。</p>
+          <button
+            type="button"
+            className="block mt-10 py-2 text-2xl w-4/5 mx-auto rounded-xl bg-sky-400 text-white"
+          >
+            ログイン
+          </button>
+          <button
+            type="button"
+            className="block mt-5 py-2 text-2xl w-4/5 mx-auto rounded-xl bg-white text-sky-400 border border-solid border-sky-400"
+          >
+            新規登録
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
