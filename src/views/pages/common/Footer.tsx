8837c1956b7967367dd396ce5fb6cd4a9a3f1254
nikawadori <nikawa.2161.t@gmail.com>
2022-11-27 14:58:37 +0900

    footerの作成

---
+++ src/views/pages/common/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+export const Footer:FC = () => {
+  return (
+    <>
+      <div className="mt-10 py-2 px-5 bg-blue-primary">
+        <p className="text-center text-slate-400">&copy; 2022 TomoMane</p>
+      </div>
+    </>
+  )
+}
